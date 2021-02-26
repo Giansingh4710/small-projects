19e65483adcf4720c6974f4739e5fcff81d9a8e7
Gian <giansingh4710@gmail.com>
2021-02-26 16:46:20 -0500

    Basiclly overall done. Just fixing small things

--- SikhStuff/GettingKeertanTracksFromAkj.py
+++ SikhStuff/GettingKeertanTracksFromAkj.py
@@ -1,15 +1,15 @@
 import urllib.request
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import time
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("--headless")
+options.add_argument={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}#my user agent
+br = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe',options=options)
 
-#options = webdriver.ChromeOptions()
-#options.headless = True
-#options.add_argument("--headless")
-#options.add_argument={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}#my user agent
-br = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')#,options=options)
 
-
-allUrls=[] #all urls for keertan pages of people
+allUrls=[] #all urls for keertan pages of people. Not in function because we loop over getKeetani Func
 def getKeertani(keertani):
     akj="https://www.akj.org/keertan.php"
     br.get(akj)
@@ -42,14 +42,39 @@ def getKeertani(keertani):
     br.find_element_by_css_selector("body > div.container > form > div > div > div > div.col-md-12.text-center.keer-top-but > input").click() #click search button
     allUrls.append(br.current_url)
 
+def getShabads(urls):
+    keertanTracks=[]
+    for i in urls:
+        br.get(i)
+        pluses=br.find_elements_by_class_name("fa-plus")
+        for plus in pluses:
+            plus.click()
+            time.sleep(0.5) 
+        atags=br.find_elements_by_tag_name("a")
+        for atag in atags:
+            link=atag.get_attribute("href")
+            if link!=None:
+                if 'Keertan' in link and "akj" in link and "mp3" in link:
+                    keertanTracks.append(link)
+        print(len(keertanTracks))
+    return keertanTracks
 
-allpeople=['bhai jaswant',"nirmal","giaan"]
+def downloadShabads(tracks):
+    c=0
+    for i in tracks:
+        c+=1
+        title=i.replace('/',"#")
+        urllib.request.urlretrieve(i,f"D:\\{c}){title[31:]}")
+        print(i)
+
+
+allpeople=['bhai jaswant',"nirmal","Bhagat"]
 for i in allpeople:
     getKeertani(i)
-print(allUrls)
-'''
-def getShabads():
-    pass
-def downloadShabads():
-    pass
-'''
+
+downloadShabads(getShabads(allUrls))
+
+
+
+
+    
