c3b8f75e34c8c27d3841bf5c4186c44363193462
Gian <giansingh4710@gmail.com>
2021-02-26 13:30:58 -0500

    removed the bug. Can now loop through many keertanins

--- SikhStuff/GettingKeertanTracksFromAkj.py
+++ SikhStuff/GettingKeertanTracksFromAkj.py
@@ -2,10 +2,10 @@ import urllib.request
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument("--headless")
-options.add_argument={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}#my user agent
+#options = webdriver.ChromeOptions()
+#options.headless = True
+#options.add_argument("--headless")
+#options.add_argument={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}#my user agent
 br = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')#,options=options)
 
 
@@ -13,25 +13,35 @@ allUrls=[]
 def getKeertani(keertani):
     akj="https://www.akj.org/keertan.php"
     br.get(akj)
-    TheKeertanDropdownMenue=br.find_element_by_css_selector("#keert_drpdwn-selectized")
-    TheKeertanDropdownMenue.send_keys(keertani)
-    optionsContainer=br.find_element_by_css_selector("body > div.container > form > div > div > div > div.col-md-3.col-xs-12 > div > div.selectize-dropdown.single.selectpicker.select > div")
-    optionsList=optionsContainer.find_elements_by_class_name("option") #taking all the ooptions from dropdown menu and put in list
-    optionsList=[i.text for i in optionsList] #only the names are in this list as before there was other stuff in the list
-    print(optionsList)
-    for i in range(len(optionsList)):
-        print(f'{i+1}) {optionsList[i]}')
-    numForKerrtani=int(input("Type the number for the keertani you want: "))-1
-    print(optionsList[numForKerrtani])
-    TheKeertanDropdownMenue.send_keys(optionsList[numForKerrtani]) #type the exact keetani in the "search keertani"
-    optionsContainer.find_element_by_class_name("option").click() #select that option
+    dropDown=br.find_element_by_css_selector("#keert_drpdwn-selectized")
+    dropDown.send_keys(keertani)
+    optCont=br.find_element_by_css_selector("body > div.container > form > div > div > div > div.col-md-3.col-xs-12 > div > div.selectize-dropdown.single.selectpicker.select > div")
+    opts=optCont.find_elements_by_class_name("option") #taking all the ooptions from dropdown menu and put in list
+    optLst=[i.text for i in opts] #only the names are in this list as before there was other stuff in the list
+    print(optLst)
+    for i in range(len(optLst)):
+        print(f'{i+1}) {optLst[i]}')
+    ind=int(input("Type the number for the keertani you want: "))-1
+    theKeertani=optLst[ind]
+    print(theKeertani)
+    dropDown.send_keys(theKeertani) #type the exact keetani in the "search keertani"
+    while True:
+        try:
+            optCont.find_element_by_class_name("option").click() #select that option
+            break
+        except:
+            dropDown.clear()
+            theKeertani=theKeertani[:-1]
+            dropDown.send_keys(theKeertani)
     br.find_element_by_css_selector("body > div.container > form > div > div > div > div.col-md-12.text-center.keer-top-but > input").click() #click search button
     print(br.current_url)
     allUrls.append(br.current_url)
         #br.get_screenshot_as_file("screenshot.png")
 
-list="sant"
-getKeertani(list)
+allpeople=['bhai jaswant',"nirmal","gian"]
+for i in allpeople:
+    getKeertani(i)
+print(allUrls)
 '''
 def getShabads():
     pass
