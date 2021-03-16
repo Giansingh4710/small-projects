3a6b41a08e360709b342ae329b11047aef2a6eaa
Gian <giansingh4710@gmail.com>
2021-03-15 23:09:19 -0400

    Can now identify what track came from what folder

--- SikhStuff/GurmatVecharToSD.py
+++ SikhStuff/GurmatVecharToSD.py
@@ -2,31 +2,37 @@ import urllib.request
 from bs4 import BeautifulSoup as bs 
 import requests
 from datetime import datetime as dt
+import os
 
 
 StupidLongVariablecounterBecauseItIsGlobal=0
-def getAllLinks(url):
+def getAllLinks(url,folder):
     res=requests.get(url)
     soup=bs(res.text, 'lxml')
     khatas=soup.find_all("table",cellpadding=4)
-    khatas=khatas[4:-2]
-    linksWithTitles={}
+    khatas=khatas[5:-2]
+    folderWithLinks={folder:[]}
     for file in khatas:
         newUrl="http://www.gurmatveechar.com/"+file.find("a").get("href")
         if "mp3" in newUrl.lower():
             global StupidLongVariablecounterBecauseItIsGlobal
             StupidLongVariablecounterBecauseItIsGlobal+=1
             title=f"{str(StupidLongVariablecounterBecauseItIsGlobal)} ) {file.text}"
-            linksWithTitles[title]=newUrl
+            folderWithLinks[folder].append(title)
+            folderWithLinks[folder].append(newUrl)
         else:
-            newLinkWithTitles=getAllLinks(newUrl)
-            linksWithTitles.update(newLinkWithTitles)
-    return linksWithTitles
+            newFolder=file.text[:-7]
+            print(newFolder)
+            newFolderWithLinks=getAllLinks(newUrl,newFolder)
+            folderWithLinks.update(newFolderWithLinks)
+    return folderWithLinks
 
 def getMBs(link):
     import re
     mb=re.compile(r"([0-9]{1,3}(\.[0-9]*)?\s(MB))")
-    khatas=getAllLinks(link)
+    khatas=getAllLinks(link,"main")
+    print(khatas.keys())
+    print(len(list(khatas.keys())))
     allMbs=""
     MBsum=0
     for i in khatas:
@@ -37,9 +43,10 @@ def getMBs(link):
     for i in khatas:
         title=i[:30]+".mp3"
         finalUrl=khatas[i]
-        #urllib.request.urlretrieve(finalUrl,f'D:\\3)GianiSantji\\{title}')
-        print(f'{title} - {finalUrl}')
-    print(f"Total MBs: {MBsum}",end="\n")
+        #urllib.request.urlretrieve(finalUrl,f'C:\\Users\\gians\\Desktop\\gurmukhi\\{title}')
+        #print(f'{title} - {finalUrl}')
+    print(f"\nTotal MBs: {MBsum}")
+    print(f"\nTotal GBs: {MBsum/1000}\n")
 
 def EnterUrl(link):
     start=str(dt.now())
@@ -53,7 +60,7 @@ def EnterUrl(link):
     print(f"Minutes: {(endSeconds-startSeconds)/60}")
     print(f"Hours: {(endSeconds-startSeconds)/(60*60)}")
 
-url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKatha"
+url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKatha%2F02_Present_Day_Katha%2FBhai_Pinderpal_Singh_%28Ludhiane_wale%29"
 EnterUrl(url)
 
 
