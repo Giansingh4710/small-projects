6b5a0e3c262b5445b0eaf18531174955c7e5cc36
Gian <giansingh4710@gmail.com>
2021-03-16 11:39:51 -0400

    Now the code will make folders of the khata is in folders.For all khata for suraj parkash from Sant Inderjeet Singh ji.

--- SikhStuff/GurmatVecharToSD.py
+++ SikhStuff/GurmatVecharToSD.py
@@ -10,19 +10,18 @@ def getAllLinks(url,folder):
     res=requests.get(url)
     soup=bs(res.text, 'lxml')
     khatas=soup.find_all("table",cellpadding=4)
-    khatas=khatas[5:-2]
+    khatas=khatas[4:-2]
     folderWithLinks={folder:[]}
     for file in khatas:
         newUrl="http://www.gurmatveechar.com/"+file.find("a").get("href")
         if "mp3" in newUrl.lower():
             global StupidLongVariablecounterBecauseItIsGlobal
             StupidLongVariablecounterBecauseItIsGlobal+=1
-            title=f"{str(StupidLongVariablecounterBecauseItIsGlobal)} ) {file.text}"
+            title=f"{str(StupidLongVariablecounterBecauseItIsGlobal).zfill(3)} ) {file.text}"
             folderWithLinks[folder].append(title)
             folderWithLinks[folder].append(newUrl)
         else:
-            newFolder=file.text[:-7]
-            print(newFolder)
+            newFolder=file.text.replace("Ji","Ji ) ") #befor when you got the text from file, it would all appear without spaces so this helps with reading
             newFolderWithLinks=getAllLinks(newUrl,newFolder)
             folderWithLinks.update(newFolderWithLinks)
     return folderWithLinks
@@ -31,22 +30,31 @@ def getMBs(link):
     import re
     mb=re.compile(r"([0-9]{1,3}(\.[0-9]*)?\s(MB))")
     khatas=getAllLinks(link,"main")
-    print(khatas.keys())
-    print(len(list(khatas.keys())))
-    allMbs=""
-    MBsum=0
-    for i in khatas:
-        allMbs+=i
-    for i in mb.findall(allMbs):
-        val=float(i[0][:-3])
-        MBsum+=val
-    for i in khatas:
-        title=i[:30]+".mp3"
-        finalUrl=khatas[i]
-        #urllib.request.urlretrieve(finalUrl,f'C:\\Users\\gians\\Desktop\\gurmukhi\\{title}')
-        #print(f'{title} - {finalUrl}')
-    print(f"\nTotal MBs: {MBsum}")
-    print(f"\nTotal GBs: {MBsum/1000}\n")
+    allMbSum=0
+    for khata in khatas:
+        titles=[khatas[khata][i] for i in range(len(khatas[khata])) if i%2==0]
+        links=[khatas[khata][i] for i in range(len(khatas[khata])) if i%2!=0]
+        print(khata+" : ",end="")
+
+        folderPath="D:\\SantInderjeetSinghJi\\"
+        if khata!="main":
+            folderPath+=khata+"\\"
+            os.mkdir(folderPath)
+        FolderMbs=""
+        MBsum=0
+        for i in titles:
+            FolderMbs+=i
+        for i in mb.findall(FolderMbs):
+            val=float(i[0][:-3])
+            MBsum+=val
+        print(MBsum)
+        allMbSum+=MBsum
+        for i in range(len(links)):
+            title=titles[i][:6]+titles[i][50:-52]+".mp3"
+            urllib.request.urlretrieve(links[i],f'{folderPath}{title}')
+            print(f'{title} - {links[i]}')
+    print(f"\nTotal MBs: {allMbSum}")
+    print(f"Total GBs: {allMbSum/1000}\n")
 
 def EnterUrl(link):
     start=str(dt.now())
@@ -60,7 +68,7 @@ def EnterUrl(link):
     print(f"Minutes: {(endSeconds-startSeconds)/60}")
     print(f"Hours: {(endSeconds-startSeconds)/(60*60)}")
 
-url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKatha%2F02_Present_Day_Katha%2FBhai_Pinderpal_Singh_%28Ludhiane_wale%29"
+url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKatha%2F02_Present_Day_Katha%2FSant_Giani_Inderjeet_Singh_%28Raqbe_wale%29%2FSri_Gurpartap_Sooraj_Parkash_Katha"
 EnterUrl(url)
 
 
