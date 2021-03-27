813d0b3e4dd10a347e789675ca58b3bad41c4b42
Gian <giansingh4710@gmail.com>
2021-03-27 10:26:40 -0400

    DOUBLE RECURSION. Now if folder is in folder, it will end up that way when downloaded!!!!

--- SikhStuff/GurmatVecharToSD.py
+++ SikhStuff/GurmatVecharToSD.py
@@ -3,43 +3,63 @@ from bs4 import BeautifulSoup as bs
 import requests
 from datetime import datetime as dt
 import os
+import re
+mb=re.compile(r"([0-9]{1,3}(\.[0-9]*)?\s((MB)|(KB)))")
 
 
-StupidLongVariablecounterBecauseItIsGlobal=0
-def getAllLinks(url,folder):
+totalFiles=0
+def getAllLinks(url,folder="main"):
     res=requests.get(url)
     soup=bs(res.text, 'lxml')
     khatas=soup.find_all("table",cellpadding=4)
     khatas=khatas[4:-2]
     folderWithLinks={folder:[]}
+    count=0 
     for file in khatas:
+        try:
+            title=file.find("font",size="2",color="0069c6").text
+        except AttributeError:
+            print("No Good. But we caught it!!")#It got the ALL the text from the drop down menu and those don't have a 'color=0069c6' attribute
+            continue
         newUrl="http://www.gurmatveechar.com/"+file.find("a").get("href")
         if "mp3" in newUrl.lower():
-            global StupidLongVariablecounterBecauseItIsGlobal
-            StupidLongVariablecounterBecauseItIsGlobal+=1
-            title=f"{str(StupidLongVariablecounterBecauseItIsGlobal).zfill(3)} ) {file.text}"
+            global totalFiles
+            totalFiles+=1
+            count+=1
+            length=file.find_all("td",align="right")
+            for td in length:
+                if "mb" in td.text.lower() or "kb" in td.text.lower():
+                    theMB=td.text
+            title=f"{str(count).zfill(3)} ) {title}"+"???"+theMB #the last part contians the MBs of the file.
             folderWithLinks[folder].append(title)
             folderWithLinks[folder].append(newUrl)
         else:
-            newFolder=file.text[:-8] ## "Volume 15 Ang 1352-143093 files" what it looks like without the -8. The last 2 nums are the numbers of files in that folder
+            newFolder=title
             newFolderWithLinks=getAllLinks(newUrl,newFolder)
-            folderWithLinks.update(newFolderWithLinks)
+            if folder=="main": # the purpose og this statement is so that when we make the folders to download the files, the folders in the folder end up in the folder lol
+                folderWithLinks.update(newFolderWithLinks)
+            else:
+                folderWithLinks[folder].append(newFolderWithLinks) 
+                #ifthere is a folder in a folder, the MAIN folder will be the key 
+                # and its keys are other dictionaies. The keys to ALL dictionaries are titles of folders 
+                # and the base value is a list of links
+                #the program will keep recusing as long as there is folder and will stop when there are
+                #mp3 files
     return folderWithLinks
 
-def getMBs(link):
-    import re
-    mb=re.compile(r"([0-9]{1,3}(\.[0-9]*)?\s(MB))")
-    khatas=getAllLinks(link,"main")
-    allMbSum=0
+allMbSum=0
+def download(khatas,thePath):
     for khata in khatas:
+        if khata!="main":
+            folderPath=thePath+khata+"\\"
+            os.mkdir(folderPath)
+            if type(khatas[khata][0])==dict:
+                listOfDict=khatas[khata]
+                for dictt in listOfDict:
+                    download(dictt,folderPath)
+                continue #so the dict of dicts dosen't keep going down
         titles=[khatas[khata][i] for i in range(len(khatas[khata])) if i%2==0]
         links=[khatas[khata][i] for i in range(len(khatas[khata])) if i%2!=0]
-        print(khata+" : ",end="")
-
-        folderPath="D:\\3. SurajParkash\\"
-        if khata!="main":
-            folderPath+=khata+"\\"
-            os.mkdir(folderPath)
         FolderMbs=""
         MBsum=0
         for i in titles:
@@ -47,25 +67,33 @@ def getMBs(link):
         for i in mb.findall(FolderMbs):
             val=float(i[0][:-3])
             MBsum+=val
-        print(MBsum)
+        global allMbSum
         allMbSum+=MBsum
+        print("\n"+khata+" : ",MBsum)
         for i in range(len(links)):
-            title=titles[i][:6]+titles[i][50:-52]+".mp3"
+            title=titles[i].split("???")[0]+".mp3"
             noNo='\/:*?"<>|' #cant name a file with any of these characters so if the title has any of these characters, the loop will replace them
             for bad in noNo:
                 if bad in title:
                     title=title.replace(bad,"#")
             urllib.request.urlretrieve(links[i],f'{folderPath}{title}')
             print(f'{title} - {links[i]}')
-    print(f"\nTotal MBs: {allMbSum}")
-    print(f"Total GBs: {allMbSum/1000}\n")
-    print(f"\nTotal MBs: {allMbSum}")
-    print(f"Total GBs: {allMbSum/1000}\n")
 
-def EnterUrl(link):
+def EnterUrl(link,path):
     start=str(dt.now())
-    getMBs(link)
+
+    if path[-1]!="\\":
+        path+="\\"
+
+    khatas=getAllLinks(link)
+    download(khatas,path)
+
     end=str(dt.now())
+
+    print(f"\nTotal MBs: {allMbSum}")
+    print(f"Total GBs: {allMbSum/1000}")
+    print("In total: "+str(totalFiles)+" total files\n")
+    
     print(f"Start: {start}")
     print(f"End: {end}",end="\n\n")
     startSeconds=(int(start[11:13])*60*60)+(int(start[14:16])*60)+int(start[17:19])
@@ -74,8 +102,9 @@ def EnterUrl(link):
     print(f"Minutes: {(endSeconds-startSeconds)/60}")
     print(f"Hours: {(endSeconds-startSeconds)/(60*60)}")
 
-url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKatha%2F02_Present_Day_Katha%2FSant_Giani_Inderjeet_Singh_%28Raqbe_wale%29%2FSri_Gurpartap_Sooraj_Parkash_Katha"
-EnterUrl(url)
+url="http://www.gurmatveechar.com/audio.php?q=f&f=%2FKeertan%2FBhai_Avtar_Singh_%28Delhi_wale%29"
+path="C:\\users\\gians\\desktop\\test"
+EnterUrl(url,path)
 
 
     
